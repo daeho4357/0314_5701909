2672ef30948cf62c8d9926f9a80df870bff41e0b
대호 양 <yangdaeho2002@gmail.com>
2024-03-19 10:22:37 +0900

    fact

--- 0314_5701909_1.c/main.c
+++ 0314_5701909_1.c/main.c
@@ -2,44 +2,55 @@
 #include <stdlib.h>
 #include <time.h>
 
+double factorial_iter(int n){
+    
+    double result = 1.0;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+double factorial_rec(int m) {
+    if (m == 0)
+        return 1.0;
+    else
+        return m * factorial_rec(m - 1);
+}
+
+long testTime(void) {
+    return 0;
+}
+
+
+
+
 int main() {
 
     clock_t start, stop;
     double duration;
+    //start = clock();
+
     start = clock();
-
-
-    int sum = 0;
-
-    for (int i = 1; i <= 100; i++) {
-
-        int ss = 1;
-
-        if (i == 1) {
-            ss = 0;
-        }
-        else
-        {
-            for (int x = 2; x < i; x++) {
-                if (i % x == 0) {
-                    ss = 0;
-                }
-            }
-        }
-
-        if (ss == 1) {
-            sum += i;
-        }
-
-    }
-
-///
-    printf("1에서 100까지 소수의 합: %d\n", sum);
-
-
+    double result_iter = factorial_iter(20);
     stop = clock();
     duration = (double)(stop - start) / CLOCKS_PER_SEC;
+
+    printf("fack_iter의 값: %f \n", result_iter);
     printf("수행시간: %f초.\n", duration);
+
+
+    start = clock();
+    double result_rec = factorial_rec(20);
+    stop = clock();
+    duration = (double)(stop - start) / CLOCKS_PER_SEC;
+
+    printf("fact_rec의 값: %f \n", result_rec);
+    printf("수행시간: %f초.\n", duration);
+
+    //stop = clock();
+    //duration = (double)(stop - start) / CLOCKS_PER_SEC;
+    //printf("수행시간: %f초.\n", duration);
 
     return 0;
 }
